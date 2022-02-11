ea3dc1b45bfcd4dd3a4dd3dcae923bc59ff8a69f
Mohamed Jasil <mohamedm@valorem.com>
2022-02-11 11:54:57 +0530

    new form project

--- angular-first-app/src/app/shopping-list/shopping-list.component.ts
+++ angular-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,18 +8,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients : Ingredient[];
+  private ingredientChangeSub : Subscription;
 
   constructor(private shoppingListService : ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.ingredientChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredient : Ingredient[]) => {
         this.ingredients = ingredient;
       }
     );
+  }
+
+  ngOnDestroy(): void {
+      this.ingredientChangeSub.unsubscribe();
   }
 
 }
