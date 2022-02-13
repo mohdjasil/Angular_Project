33026f7104fd73f2e01cfa2541673f224ba7690c
Mohamed Jasil <mohamedm@valorem.com>
2022-02-13 20:09:52 +0530

    added submit buton to display entered details

--- form-eg/src/app/app.component.ts
+++ form-eg/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,46 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm : NgForm
+  defaultQuestionValue = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   username: 'jachu',
+    //   email: '',
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      username: 'jachu'
+    });
   }
 
-  onSubmit(form : NgForm){
-    console.log(form.value);
+  // onSubmit(form : NgForm){
+  //   console.log(form.value);
+  // }
+
+  onSubmit(){
+    debugger;
+    console.log(this.signupForm.value);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
