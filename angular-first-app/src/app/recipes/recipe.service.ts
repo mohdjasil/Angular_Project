05f26eef345639e355b5281549133a5b2cdc3df6
Mohamed Jasil <mohamedm@valorem.com>
2022-02-15 18:18:01 +0530

    recipe site form completed

--- angular-first-app/src/app/recipes/recipe.service.ts
+++ angular-first-app/src/app/recipes/recipe.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService{
-
+    recipesChanged = new Subject<Recipe[]>();
     private _recipes : Recipe[] = [
         new Recipe(
             'Burger',
@@ -39,5 +40,20 @@ export class RecipeService{
 
     addIngredientToShoppingList(ingredients : Ingredient[]){
         this.shoppingListService.addIngredientFromRecipe(ingredients);
+    }
+
+    addRecipe(recipe: Recipe){
+        this._recipes.push(recipe);
+        this.recipesChanged.next(this._recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe){
+        this._recipes[index] = newRecipe;
+        this.recipesChanged.next(this._recipes.slice());
+    }
+
+    deleteRecipe(index: number){
+        this._recipes.splice(index, 1);
+        this.recipesChanged.next(this._recipes.slice());
     }
 }
