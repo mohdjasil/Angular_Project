2b3b5d68d84fa716e3c4a7a234016200fd0437de
Mohamed Jasil <mohamedm@valorem.com>
2022-02-16 16:27:41 +0530

    added pipe example

--- pipe-eg/src/app/app.component.ts
+++ pipe-eg/src/app/app.component.ts
@@ -28,15 +28,25 @@ export class AppComponent {
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
-      status: 'stable',
+      status: 'critical',
       started: new Date(15, 1, 2017)
     }
   ];
+  filteredStatus = '';
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'medium',
+      name: 'Test Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
   }
 }
