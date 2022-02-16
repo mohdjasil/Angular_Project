770aa8997a1a4348cd49c0fcc113dbd2f9dd86a6
Mohamed Jasil <mohamedm@valorem.com>
2022-02-16 16:29:59 +0530

    added pure pipe state

--- pipe-eg/src/app/filter.pipe.ts
+++ pipe-eg/src/app/filter.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'filter'
+    name: 'filter',
+    pure: false //this makes sure that pipe gets refreshed on every change.
 })
 export class FilterPipe implements PipeTransform{
     transform(value: any, filterString: any, propName: any ) {
