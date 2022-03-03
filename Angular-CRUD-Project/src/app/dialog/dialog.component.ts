9110667d1a16e5bb0588b5fbc6457f82ebc3d056
Mohamed Jasil <mohamedm@valorem.com>
2022-03-03 20:42:46 +0530

    added dialog form to add product, added header

---
+++ Angular-CRUD-Project/src/app/dialog/dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  freshnessList = [
+    "Brand New",
+    "Second Hand",
+    "Refurbished"
+  ]
+
+  productForm !: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      price: ['', Validators.required],
+      comment: ['', Validators.required],
+      date: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.productForm);
+  }
+}
